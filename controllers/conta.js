76d9123b7af959cdbd205ca32aba45edb914dfc1
Ricardo Gomes <desk467@gmail.com>
2017-12-10 18:54:32 -0300

    Corrigindo bug em cadastro

--- controllers/conta.js
+++ controllers/conta.js
@@ -24,8 +24,8 @@ module.exports = function (app, models, m) {
 
     app.get('/cadastro', m.usuario_deslogado, function (req, res) {
 
-        models.Estado.recuperarTudo(function(erro, estados){
-            if(!erro){
+        models.Estado.recuperarTudo(function (erro, estados) {
+            if (!erro) {
                 res.render('conta/cadastro', { erro: null, estados })
             }
         })
@@ -34,7 +34,10 @@ module.exports = function (app, models, m) {
     app.post('/cadastro', m.usuario_deslogado, function (req, res) {
         models.Usuario.inserir(req.body, function (erro, usuario) {
             if (erro) {
-                res.render('conta/cadastro', { erro })
+                models.Estado.recuperarTudo(function (erro_estado, estados) {
+                    res.render('conta/cadastro', { erro, estados })
+                })
+
             } else {
                 req.session.usuario = usuario
                 req.session.save()
